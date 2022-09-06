f5774123512e4089141e7a3ccd6cd806361538a4
Jasper Vaneessen <jasper.vaneessen@gmail.com>
2022-09-06 11:04:27 +0200

    chore: comment

--- src/authorization/AllStaticReader.ts
+++ src/authorization/AllStaticReader.ts
@@ -12,6 +12,7 @@ export class AllStaticReader extends PermissionReader {
   private readonly permissions: Permission;
 
   // A triggering comment change 1
+  // More comments
   public constructor(allow: boolean) {
     super();
     this.permissions = Object.freeze({
