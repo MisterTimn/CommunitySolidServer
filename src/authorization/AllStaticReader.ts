3e655d7f0ad8677ac1795581d5ddb043b8105a1c
Jasper Vaneessen <jasper.vaneessen@gmail.com>
2022-09-06 11:09:31 +0200

    chore: remove comments

--- src/authorization/AllStaticReader.ts
+++ src/authorization/AllStaticReader.ts
@@ -11,8 +11,6 @@ import type { Permission, PermissionMap, PermissionSet } from './permissions/Per
 export class AllStaticReader extends PermissionReader {
   private readonly permissions: Permission;
 
-  // A triggering comment change 1
-  // More comments
   public constructor(allow: boolean) {
     super();
     this.permissions = Object.freeze({
